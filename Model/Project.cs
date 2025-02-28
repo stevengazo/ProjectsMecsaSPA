a13e18222bc93c8031670b65d7ef700d98ce17bf
Steven Gazo M <43178863+stevengazo@users.noreply.github.com>
2025-02-28 08:39:33 -0600

    Fix (addproject) fixing the issue of the projectid

--- Model/Project.cs
+++ Model/Project.cs
@@ -8,7 +8,6 @@ namespace ProjectsMecsaSPA.Model
     public class Project
     {
         [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.None)] // No será autoincremental
         public int ProjectId { get; set; }
 
         [Required(ErrorMessage = "El título es obligatorio.")]
