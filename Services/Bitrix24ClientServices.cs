a3c9285971a444d0cb5687dcc09b5525abdee7f6
Steven Gazo M <43178863+stevengazo@users.noreply.github.com>
2025-03-07 17:34:36 -0600

    Feat (bitrix24): add commentaries in the taks in bitrix

---
+++ Services/Bitrix24ClientServices.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+
+namespace ProjectsMecsaSPA.Services
+{
+    public class Bitrix24ClientServices
+    {
+        private  readonly string UrlTaskAdd = "";
+
+        public async Task SendTaskCommentAsync(int taskId, string message)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                var requestBody = new
+                {
+                    fields = new
+                    {
+                        AUTHOR_ID = 186,
+                        POST_MESSAGE = $"SPA - comentario automático: {message}" 
+                    }
+                };
+
+                string json = System.Text.Json.JsonSerializer.Serialize(requestBody);
+                HttpContent content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                HttpResponseMessage response = await client.PostAsync($"{UrlTaskAdd}?TASKID={taskId}", content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine("Comentario enviado exitosamente.");
+                }
+                else
+                {
+                    Console.WriteLine($"Error en el envío: {response.StatusCode} - {await response.Content.ReadAsStringAsync()}");
+                }
+            }
+        }
+    }
+}
