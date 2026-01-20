e74f8dce38215cb4769a0576a72b9e4ca579a332
Steven Gazo M <43178863+stevengazo@users.noreply.github.com>
2026-01-20 17:03:15 -0600

    chart

---
+++ Model/LeadChartModel.cs
@@ -0,0 +1,14 @@
+﻿namespace ProjectsMecsaSPA.Model
+{
+public class LeadChartModel
+    {
+        public int LeadId { get; set; }
+        public string CompanyName { get; set; }
+        public DateTime CreationDate { get; set; }
+        public string Seller { get; set; }
+        public string Sector { get; set; }
+        public string Origin { get; set; }
+        public string State { get; set; }
+        public string Request { get; set; }
+    }
+}
